0a74d53085ede3c510edc96c964470af914335c4
erfolen <dlababy@mail.ru>
2020-10-18 22:16:30 +0300

    init: create fuction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  let result = [];
+  matrix.forEach (
+    function (val,ind,arr) {
+      if (ind % 2 != 0) {
+        val.reverse(); 
+        result = result.concat(val); 
+      } else {
+        result = result.concat(val);
+      }
+    }
+  )
+  return result;
 }
